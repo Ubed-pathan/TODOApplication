91b48ed2e19ad1c5e4457a83387d71b33a70a553
ubed pathan <ubedpathan818@gmail.com>
2024-12-16 11:25:10 +0530

    Complete Todo App

--- Todo Server/src/main/java/com/ubedpathan/TodoApp/TodoAppApplication.java
+++ Todo Server/src/main/java/com/ubedpathan/TodoApp/TodoAppApplication.java
@@ -1,6 +1,5 @@
 package com.ubedpathan.TodoApp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
--- Todo Server/src/test/java/com/ubedpathan/TodoApp/TodoAppApplicationTests.java
+++ Todo Server/src/test/java/com/ubedpathan/TodoApp/TodoAppApplicationTests.java
@@ -1,21 +1,16 @@
-package com.ubedpathan.TodoApp;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TodoAppApplicationTests {
-
-	@BeforeAll
-	public static void setup() {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-	}
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ubedpathan.TodoApp;
+//
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TodoAppApplicationTests {
+//
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
